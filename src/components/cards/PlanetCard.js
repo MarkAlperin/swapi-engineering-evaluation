b5585b8106c87313164d4f908de8838ea6da1879
MarkAlperin <mark.t.alperin@gmail.com>
2022-06-27 18:22:15 -0500

    made PlanetCard component

---
+++ src/components/cards/PlanetCard.js
@@ -0,0 +1,52 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import AppContext from "../../context/appContext";
+
+const PlanetCard = ({ planet }) => {
+  const appCtx = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const planetClickHandler = () => {
+    appCtx.setCurrentPlanet(planet);
+    navigate("/planet-page");
+  };
+
+  return (
+    <PlanetDisplayContainer onClick={planetClickHandler}>
+      <h4>{planet.name}</h4>
+      <StyledSpan>
+        <p>Terrain: {planet.terrain}</p>
+
+        <StyledP>Population: {planet.population}</StyledP>
+
+        <StyledP>Residents: {planet.residents.length}</StyledP>
+      </StyledSpan>
+    </PlanetDisplayContainer>
+  );
+};
+
+export default PlanetCard;
+
+const PlanetDisplayContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  width: 60%;
+  border: 2px solid black;
+  border-radius: 10px;
+  hover: pointer;
+`;
+
+const StyledSpan = styled.span`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const StyledP = styled.p`
+  margin-left: 10px;
+  margin-right: 10px;
+`;
