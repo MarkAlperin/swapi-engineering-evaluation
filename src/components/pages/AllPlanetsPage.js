cc0687d4705743ce430b73f1613e24aeeff2609d
MarkAlperin <mark.t.alperin@gmail.com>
2022-06-27 18:16:04 -0500

    basic allPlanetPage/PlanetCard functionality

--- src/components/pages/AllPlanetsPage.js
+++ src/components/pages/AllPlanetsPage.js
@@ -1,17 +1,36 @@
 import React, { useContext } from "react";
+import styled from "styled-components";
 
 import AppContext from "../../context/appContext";
+import PlanetCard from "../cards/PlanetCard";
 
 const AllPlanetsPage = () => {
   const appCtx = useContext(AppContext);
+  const height = window.innerHeight * 0.8;
 
-  console.log(appCtx.searchTerm);
+  const planetClickHandler = (planet) => {
+    // appCtx.setCurrentPlanet()
+    console.log(planet)
+  }
+
   return (
     <div>
-        <p>This is the All Planets Page</p>
-
+      <h1>This is the All Planets Page</h1>
+      <PlanetsContainer height={height}>
+        {appCtx.planets.map((planet, idx) => (
+          <PlanetCard key={idx} planet={planet} planetClickHandler={planetClickHandler}/>
+        ))}
+      </PlanetsContainer>
     </div>
   );
 };
 
 export default AllPlanetsPage;
+
+const PlanetsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: ${({height}) => height}px;
+  overflow-y: scroll;
+`;
